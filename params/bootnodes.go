73769103169a8ddf0dded4d07db89dadc7525216
eden120 <eden.net120@gmail.com>
2018-03-12 08:52:32 -0700

    wallet done

---
+++ params/bootnodes.go
@@ -0,0 +1,40 @@
+// Copyright 2015 The go-ethereum Authors
+// This file is part of the go-ethereum library.
+//
+// The go-ethereum library is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// The go-ethereum library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with the go-ethereum library. If not, see <http://www.gnu.org/licenses/>.
+
+package params
+
+// MainnetBootnodes are the enode URLs of the P2P bootstrap nodes running on
+// the main Ethereum network.
+var MainnetBootnodes = []string{
+	// Ethereum Foundation Go Bootnodes
+	"enode://7c326191ab5be92acf702d095e5402a50bace5b751d441a932a9bb24cbef8b7733116645dd18eea9f6e94da804f1bd4112bb7996b2288d1228439ed7243eae89@67.207.92.186:30403", // IE
+}
+
+// TestnetBootnodes are the enode URLs of the P2P bootstrap nodes running on the
+// Ropsten test network.
+var TestnetBootnodes = []string{}
+
+// RinkebyBootnodes are the enode URLs of the P2P bootstrap nodes running on the
+// Rinkeby test network.
+var RinkebyBootnodes = []string{}
+
+// RinkebyV5Bootnodes are the enode URLs of the P2P bootstrap nodes running on the
+// Rinkeby test network for the experimental RLPx v5 topic-discovery network.
+var RinkebyV5Bootnodes = []string{}
+
+// DiscoveryV5Bootnodes are the enode URLs of the P2P bootstrap nodes for the
+// experimental RLPx v5 topic-discovery network.
+var DiscoveryV5Bootnodes = []string{}
